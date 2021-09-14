18d3b84e7041aad75b5b302df18481f0527d9424
Abdulhakimzatar <hakimzatar@gmail.com>
2021-09-14 10:47:22 +0300

    v1 route

--- src/data/database.js
+++ src/data/database.js
@@ -5,7 +5,7 @@ const pg = require('pg');
 require('dotenv').config();
 
 //--------------------------------// Esoteric Resources \\------------------------------\\
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || 'postgresql://snake:snake1337@198.84.76.250:5432/pwc';
 
 //----------------------------------// Export The Module \\----------------------------------\\
 module.exports = new pg.Client(connectionString);
